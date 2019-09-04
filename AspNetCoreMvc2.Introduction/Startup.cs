574012aadd524cb1f405e1772b07d31837dcc912
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-04 13:58:04 +0300

    Mvc configurations was added

--- AspNetCoreMvc2.Introduction/Startup.cs
+++ AspNetCoreMvc2.Introduction/Startup.cs
@@ -15,6 +15,7 @@ namespace AspNetCoreMvc2.Introduction
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +26,12 @@ namespace AspNetCoreMvc2.Introduction
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //middleware
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default", template: "{controller=home}/{action=index}/{id?}"
+                    );
             });
         }
     }

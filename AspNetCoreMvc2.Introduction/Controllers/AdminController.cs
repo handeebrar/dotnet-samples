f7a881e40ed26a60cd95d6b0396674a202c21a8d
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-05 15:57:28 +0300

    Attribute routing

---
+++ AspNetCoreMvc2.Introduction/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvc2.Introduction.Controllers
+{
+    //eğer sana admin diye bir req gelirse sen çalış
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        //admin yazdığımızda default olarak save operasyonunun gelmesini isteyelim
+        [Route("")]
+        public string Save()
+        {
+            return "Saved";
+        }
+
+        [Route("delete/{id?}")]
+        public string Delete(int id=0)
+        {
+            return String.Format("Deleted {0}",id);
+        }
+
+        [Route("update")]
+        public string Update()
+        {
+            return "Update";
+        }
+    }
+}

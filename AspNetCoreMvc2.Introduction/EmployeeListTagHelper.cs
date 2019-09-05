5412a3134b8af75b545c7d1435579d91be33d5c7
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-05 14:25:44 +0300

    Custom TagHelper was added

---
+++ AspNetCoreMvc2.Introduction/EmployeeListTagHelper.cs
@@ -0,0 +1,41 @@
+﻿using AspNetCoreMvc2.Introduction.Entities;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvc2.Introduction.TagHelpers
+{
+    [HtmlTargetElement("employee-list")]
+    public class EmployeeListTagHelper : TagHelper
+    {
+        private List<Employee> _employees;
+        public EmployeeListTagHelper()
+        {
+            _employees = new List<Employee>
+            {
+                new Employee{Id=1, FirstName="A", LastName="B", CityId=6 },
+                new Employee{Id=1, FirstName="C", LastName="D", CityId=6 },
+                new Employee{Id=1, FirstName="E", LastName="F", CityId=34 }
+            };
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            var query = _employees;
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var employee in query)
+            {
+                stringBuilder.AppendFormat("<h2><a href='/employee/detail/{0}'>{1}</a></h2>",employee.Id,employee.FirstName);
+            }
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+            base.Process(context, output);
+        }
+    }
+}

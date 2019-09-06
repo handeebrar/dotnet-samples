60559af221f97ff9caf74c14bb83d5662518cd6b
Hande Ebrar GÜNEŞDOĞDU <hande_ebrar@hotmail.com>
2019-09-06 14:00:47 +0300

    View Components

---
+++ AspNetCoreMvc2.Introduction/Models/StudentListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AspNetCoreMvc2.Introduction.Entities;
+
+namespace AspNetCoreMvc2.Introduction
+{
+    class StudentListViewModel
+    {
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ AspNetCoreMvc2.Introduction/ViewComponents/StudentListViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreMvc2.Introduction.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvc2.Introduction.ViewComponents
+{
+    public class StudentListViewComponent : ViewComponent
+    {
+        SchoolContext _context;
+        public StudentListViewComponent(SchoolContext context)
+        {
+            _context = context;
+        }
+        public ViewViewComponentResult Invoke(String filter)
+        {
+            //view'a model göndermemiz gerekir
+            return View(new StudentListViewModel
+            {
+                Students = _context.Students.Where(s=>s.FirstName.ToLower().Contains(filter)).ToList()
+            });
+        }
+    }
+}
